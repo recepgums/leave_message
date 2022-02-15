b010f916d75c0a9139571e1cba3a66bdb9bee2b0
Recep Gumus <recepgumusyasin@gmail.com>
2022-02-15 23:35:20 +0300

    mypost test6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,28 @@
 <?php
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 Route::get('/', 'GlobalController@home_page')->name('home-page');
+Route::get('/seeder', function (){
+    for ($x = 0; $x <= 10; $x++) {
+        $fileArray = [
+            'https://anonymupload-mmgmf.s3.eu-west-3.amazonaws.com/public/global_files/tltUFJx4915wdCaG1D9J1O1bjJMml13XRpF0nUiD.jpg',
+            'https://anonymupload-mmgmf.s3.eu-west-3.amazonaws.com/public/global_files/35OqBiMBMUhuIqt1Tjxxji1ffc1xlneoMvLeTXwL.zip',
+            'https://anonymupload-mmgmf.s3.eu-west-3.amazonaws.com/public/global_files/ENCAqOQqszceXsRLDJZgkCs7oFEeQ3YcKwwfkLJf.png',
+            'https://anonymupload-mmgmf.s3.eu-west-3.amazonaws.com/public/global_files/XpVeohUVa1PkSHBbcRkgI214XJI8c49mNldTnThK.txt',
+            'https://anonymupload-mmgmf.s3.eu-west-3.amazonaws.com/public/global_files/ftLl55rLfG0nZVL34rdKdyZfxs3x8un5Z4di0SRL.txt'
+        ];
+        $rand =random_int(0,1);
+        \App\GlobalRoomMessages::create([
+            'title' => 'Test '. random_int(100,1000),
+            'file_name' => $rand ?  array_rand($fileArray) : null,
+            'password' => $rand ? Hash::make('12345678') : null,
+            'user_id' => $rand ? 1: null,
+            'link' => env('APP_URL') . "/f/" . Str::random(20)
+        ]);
+    }
+});
 Route::get('/f/{link}', 'GlobalController@getFileByLink');
 Route::post('/f/download', 'GlobalController@getFileByLinkCheck')->name('download_file_attempt');
 Route::view('/start', 'start')->name('start-page');
